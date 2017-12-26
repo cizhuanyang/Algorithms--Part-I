2f016d889ee39835be5bec84889f2435daffaf3e
FrouFrou <yangyang55@outlook.com>
2017-12-26 13:52:40 +0800

    add part of RandomizedQueue

---
+++ src/RandomizedQueuesAndDeques/RandomizedQueue.java
@@ -0,0 +1,35 @@
+package RandomizedQueuesAndDeques;
+
+import java.awt.ItemSelectable;
+
+public class RandomizedQueue<Item> {
+	private Item[] items;
+	private int size;
+	public RandomizedQueue() {
+		items=(Item[])new Object[1];
+		size=0;
+	}
+	public boolean isEmpty() {
+		return size==0;
+	}
+	public void add(Item item) {
+		
+	}
+	public Item remove() {
+		
+	}
+	static void resizeStorage(int newsize) {
+		Item [] 
+	}
+	static void halveStorage() {
+		
+	}
+	static void doubleStorage() {
+		resizeStorage();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
